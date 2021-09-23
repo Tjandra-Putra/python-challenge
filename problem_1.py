dcd692622a17f6e6bdcde9a747e4de2b0e316579
Tjandra Putra <57522674+Tjandra-Putra@users.noreply.github.com>
2021-09-23 21:08:27 +0800

    learning process

--- problem_1.py
+++ problem_1.py
@@ -1,5 +1,6 @@
 # Separate each module grade from the string e.g C+, A, C, D- 
 
+# ================= Method 1 - Me =================
 grade = "C+ACD-"
 
 grade_char_list = list(grade)
@@ -25,6 +26,46 @@ for index in range(0, len(grade_char_list)):
 
 
 print(formatted_grade_list)
+
+
+# ================= Method 2 - Most efficient By Andy the coder =================
+string = "C+ACD-"
+grades = []
+for i in range(0,len(string)):
+    if((i+1) < len(string) and string[i+1] in '+-*'):
+        grades.append("{}{}".format(string[i],string[i+1]))
+    elif(string[i]  in '+-*' ):
+        continue
+    else:
+        grades.append(string[i])
+    
+print(grades)
+        
+# ================= Method 3 - GW Senior =================
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+grades = []
+
+current_grade = ""
+
+string = "C+ACD-"
+
+for char in string:
+    if char == "+" or char == "-":
+        current_grade += char
+        grades.append(current_grade)
+        current_grade = ""
+        
+    elif len(current_grade) == 1 and ALPHABETS.find(char) > -1:
+        grades.append(current_grade)
+        current_grade = ""
+        current_grade += char
+        
+    else:
+        current_grade += char
+
+print(grades)
+        
+        
     
     
     
